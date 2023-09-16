91619569287d17e1c71f5a32ddce6cbf4512b493
LimberthStyle <202001421b@utea.edu.pe>
2023-09-15 23:48:47 -0500

    Commit 1

---
+++ BD.php
@@ -0,0 +1,9 @@
+<?php
+
+$servidor ="localhost";
+$usuario = "root";
+$clave = "2003";
+$cn = mysqli_connect($servidor, $usuario, $clave, "BD_INFORMATICA");
+
+//mysqli_select_db($cn,"BD_INFORMATICA");
+?>
---
+++ Index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>Listado de Tareas</title>
+</head>
+
+<body>
+    <header>
+        <center>
+            <h2>Login de usuarios</h2>
+        </center>
+    </header>
+    
+    <section>
+        <center>
+            <img src="IMAGES/User.jpg" alt="Usuario" width="150">
+            
+            <table width="200" class="table table-hover">
+                <tr>
+                    <td>
+                        USUARIO:
+                    </td>
+                    <td>
+                        <input type="text" placeholder="Usuario" required name="txtUsuario">
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        PASSWORD:
+                    </td>
+                    <td>
+                        <input type="text" placeholder="Password" required name="txtPassword">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type="submit" value="Ingresar" name="btnIngresar">
+                    </td>
+                </tr>
+
+            </table>
+        </center>
+    </section>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
